9e3fbf478585b082315f176f0a6845efdc0fa4f5
Ruan <slidruan@gmail.com>
2026-01-08 15:20:36 -0300

    projeto finalizado

---
+++ script.js
@@ -0,0 +1,8 @@
+function redes() { 
+    const box = document.getElementById('share-box')
+    const op = document.querySelector('.perfil button')
+
+    box.classList.toggle('ativo')
+    op.classList.toggle ('ativo')
+
+}
